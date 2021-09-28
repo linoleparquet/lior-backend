9c30faa2409b4aff00e4acfb00bc1b386e37e96b
linoleparquet <layanilino@gmail.com>
2021-09-28 09:57:46 +0200

    feat: CORS: allow all

--- src/main/java/fr/lino/layani/lior/configuration/SpringConfiguration.java
+++ src/main/java/fr/lino/layani/lior/configuration/SpringConfiguration.java
@@ -15,7 +15,9 @@ public class SpringConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*");
             }
         };
     }
