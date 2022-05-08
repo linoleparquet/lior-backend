b311ee4ee47b469a9c7d6b77647c88428f336f11
linoleparquet <layanilino@gmail.com>
2022-05-08 10:58:02 -0400

    migrate to postgres
    	modified:   Dockerfile
    	modified:   README.md
    	modified:   pom.xml
    	new file:   src/main/resources/application-test.properties
    	modified:   src/main/resources/application.properties
    	modified:   src/test/java/fr/lino/layani/lior/LiorApplicationTests.java

--- src/test/java/fr/lino/layani/lior/LiorApplicationTests.java
+++ src/test/java/fr/lino/layani/lior/LiorApplicationTests.java
@@ -2,8 +2,10 @@ package fr.lino.layani.lior;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LiorApplicationTests {
 
 	@Test
